2f2dff7470c07eec17b5789a1271e4d4ad3befd5
Stanoid <mujahidwadmohammad@gmail.com>
2022-01-05 13:22:31 +0300

    chilcat removal

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,18 +8,5 @@ module.exports = [
   'strapi::body',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  
 ];
