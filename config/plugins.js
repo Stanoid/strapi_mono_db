64a249a588beb65dc9cb4c72143cf05f1d1c649a
Stanoid <mujahidwadmohammad@gmail.com>
2022-01-05 13:15:17 +0300

    added promotion colection

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name:"strapimedia" ,
+        api_key: "174851433522577",
+        api_secret: "zaNJC3aDs2078QrIeA5CBxJWVlU",
+      },
+    },    
+  });
