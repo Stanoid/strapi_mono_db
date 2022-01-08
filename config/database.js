fb0e20cf9b6f25127fdec0ab77a207c57c678d69
Stanoid <mujahidwadmohammad@gmail.com>
2022-01-09 00:20:50 +0300

    Stock victory

--- config/database.js
+++ config/database.js
@@ -9,5 +9,18 @@ module.exports = ({ env }) => ({
       password: env('DATABASE_PASSWORD', '11235813mJ'),
       ssl: env.bool('DATABASE_SSL', true),
     },
+    
   },
+  // connection: {
+  //   client: 'mysql',
+  //   connection: {
+  //     host: env('DATABASE_HOST', 'localhost'),
+  //     port: env.int('DATABASE_PORT', 3306),
+  //     database: env('DATABASE_NAME', 'strapi'),
+  //     user: env('DATABASE_USERNAME', 'root'),
+  //     password: env('DATABASE_PASSWORD', ''),
+  //     ssl: env.bool('DATABASE_SSL', false),
+  //   },
+    
+  // },
 });
