7959b8988425ebc4fd6aa870c5ffa95c730a842b
Stanoid <mujahidwadmohammad@gmail.com>
2022-06-09 10:24:47 +0300

    database migration

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
     client: 'mysql',
     connection: {
       host: env('DATABASE_HOST', '185.166.188.103'),
+      
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'u985780343_bendari'),
       user: env('DATABASE_USERNAME', 'u985780343_stanoid'),
