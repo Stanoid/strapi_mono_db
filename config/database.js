bc006f8923866057046df29325b30da34c03997b
Stanoid <mujahidwadmohammad@gmail.com>
2024-02-02 10:40:49 +0300

    far cry

--- config/database.js
+++ config/database.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', '156.67.72.51'),
 
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'u897671277_Addon'),
-      user: env('DATABASE_USERNAME', 'u897671277_Admin'),
-      password: env('DATABASE_PASSWORD', 'u897671277_Admin'),
+      database: env('DATABASE_NAME', 'u897671277_demo'),
+      user: env('DATABASE_USERNAME', 'u897671277_admin'),
+      password: env('DATABASE_PASSWORD', '11235813mJ'),
       ssl: env.bool('DATABASE_SSL', true),
     },
 
